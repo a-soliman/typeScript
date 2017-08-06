054f214514d1b8903adb2ddf11100cf193e4d333
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-08-06 12:38:14 -0700

    practicing default param funcs in ES6

--- script.ts
+++ script.ts
@@ -1,13 +1,22 @@
-let myName: string = "Ahmed";
-let myAge: number = 29;
-let anything: any;
-anything = 12;
+// let myName: string = "Ahmed";
+// let myAge: number = 29;
+// let anything: any;
+// anything = 12;
 
-let controlMe = (isTrue: boolean, somthingElse: boolean) => {
-	let result: number;
-	if(isTrue) {
-		result =12;
+// let controlMe = (isTrue: boolean, somthingElse: boolean) => {
+// 	let result: number;
+// 	if(isTrue) {
+// 		result =12;
+// 	}
+// 	return result;
+// }
+// console.log('hi')
+
+// Default Pars in functions
+const countdown = (start: number = 10): void => {
+	while(start > 0) {
+		start--;
 	}
-	return result;
+	console.log("Done! ", start);
 }
-console.log('hi')
+console.log(countdown())
