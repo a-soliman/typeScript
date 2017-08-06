0a62729742127a88d0ee212578beee97c2c63cd3
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-08-06 12:13:55 -0700

    started the ts practice

---
+++ script.ts
@@ -0,0 +1,13 @@
+let myName: string = "Ahmed";
+let myAge: number = 29;
+let anything: any;
+anything = 12;
+
+let controlMe = (isTrue: boolean, somthingElse: boolean) => {
+	let result: number;
+	if(isTrue) {
+		result =12;
+	}
+	return result;
+}
+console.log('hi')
