f673365fc337bd7ff7cdba926ceef6668eb97666
Ahmed Soliman <ahmedsoliman@Ahmed-Solimans-MacBook-Pro.local>
2017-08-16 13:00:57 -0700

    some changes

--- script.js
+++ script.js
@@ -1,47 +1,4 @@
 "use strict";
-<<<<<<< HEAD
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Person = (function () {
-    function Person(name, username) {
-        this.username = username;
-        this.age = 30;
-        this.name = name;
-    }
-    Person.prototype.printAge = function () {
-        console.log(this.age);
-    };
-    Person.prototype.setType = function (type) {
-        this.type = type;
-        console.log(this.type);
-    };
-    return Person;
-}());
-var person = new Person('Max', 'max');
-person.printAge();
-person.setType('something');
-// Inhritance 
-var Some = (function (_super) {
-    __extends(Some, _super);
-    function Some(username) {
-        var _this = _super.call(this, "Max", username) || this;
-        _this.name = "Max";
-        _this.age = 31;
-        return _this;
-    }
-    return Some;
-}(Person));
-var some = new Some('max');
-console.log(some);
-=======
 // class Person {
 // 	name: string;
 // 	private type: string;
@@ -127,4 +84,3 @@ var rectangle_1 = require("./math/rectangle");
 console.log(Circle.PI);
 console.log(Circle.claculateCircumference(10));
 console.log(rectangle_1.calculateRectangle(20, 10));
->>>>>>> b893699c8d2377b4d554908a0cc566800759736e
