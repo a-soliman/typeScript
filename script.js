23c2410fc5093270cb639d878f589bab45261d61
Ahmed Soliman <ahmedsoliman@Ahmed-Solimans-MacBook-Pro.local>
2017-08-16 09:14:32 -0700

    something

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Person = (function () {
     function Person(name, username) {
         this.username = username;
@@ -17,3 +27,16 @@ var Person = (function () {
 var person = new Person('Max', 'max');
 person.printAge();
 person.setType('something');
+// Inhritance 
+var Some = (function (_super) {
+    __extends(Some, _super);
+    function Some(username) {
+        var _this = _super.call(this, "Max", username) || this;
+        _this.name = "Max";
+        _this.age = 31;
+        return _this;
+    }
+    return Some;
+}(Person));
+var some = new Some('max');
+console.log(some);
