489135904c4359562d80a172bfc6440b7692b0e1
Ahmed Soliman <ahmedsoliman@Ahmed-Solimans-MacBook-Pro.local>
2017-08-16 13:54:19 -0700

    halfway with generics

--- generics.ts
+++ generics.ts
@@ -1,1 +1,27 @@
-console.log('Gen')
+// Simple Generic
+
+function echo(data: any) {
+	return data;
+}
+
+console.log(echo('ddd'))
+console.log(echo(29))
+console.log(echo({name: 'Ahmed', age:29}))
+
+// Better Generic
+
+function betterEcho<T>(data: T) {
+	return data;
+}
+
+console.log(betterEcho('ahmed').length)
+console.log(betterEcho(29))
+console.log(betterEcho({name: 'Ahmed', age:29}))
+
+
+// Built-in Generic
+const testResults: Array<number> = [];
+
+testResults.push(22);
+
+console.log(testResults)
